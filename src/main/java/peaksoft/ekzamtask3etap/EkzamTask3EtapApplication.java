167a6a0251bef79007e37049f7d0685cdcd4ac9b
eldiuar <avazbekuulualdiar326@gmail.com>
2024-06-30 15:02:04 +0300

    initial commit

---
+++ src/main/java/peaksoft/ekzamtask3etap/EkzamTask3EtapApplication.java
@@ -0,0 +1,12 @@
+package peaksoft.ekzamtask3etap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkzamTask3EtapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EkzamTask3EtapApplication.class, args);
+    }
+}
---
+++ src/test/java/peaksoft/ekzamtask3etap/EkzamTask3EtapApplicationTests.java
@@ -0,0 +1,13 @@
+package peaksoft.ekzamtask3etap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkzamTask3EtapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
